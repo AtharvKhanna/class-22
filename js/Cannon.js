a97aa92d1014d95863af4ee15b50cac4c813ad7b
AtharvKhanna <87224457+AtharvKhanna@users.noreply.github.com>
2022-04-14 13:40:39 +0530

    Add files via upload

---
+++ js/Cannon.js
@@ -0,0 +1,28 @@
+class Cannon {
+  constructor(x, y, width, height, angle) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;
+    this.image = loadImage("assets/canon.png")
+    this.image1 = loadImage("assets/cannonBase.png")
+    
+
+  }
+  
+  display(){
+//code to create canon top
+
+push()
+imageMode(CENTER);
+image(this.image,this.x,this.y,this.width,this.height);
+pop()
+
+//code to create canon bottom
+
+image(this.image1,70,20,200,200);
+noFill();
+
+  }
+}
